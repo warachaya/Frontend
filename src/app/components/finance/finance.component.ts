539dd251a91cc29fc8956c2d2f3f323290f5d971
vwaravy <Warachaya_v@hotmail.cm>
2021-05-24 03:35:54 +0700

    update edok

--- src/app/components/finance/finance.component.ts
+++ src/app/components/finance/finance.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
+import { Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-finance',
@@ -11,5 +13,35 @@ export class FinanceComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  barChartOptions: ChartOptions = {
+    responsive: true,
+  };
+  barChartLabels: Label[] = ['SaleOffice', 'Inbound WB', 'Diesel Bay', 'Gasohol Bay', 'Outbound WB'];
+  barChartType: ChartType = 'bar';
+  barChartLegend = true;
+  barChartPlugins = [];
+  
+  barChartData: ChartDataSets[] = [
+    { data: [45, 37, 60, 70, 46, 33], 
+      label: 'Best Fruits',
+      backgroundColor: "rgba(219, 80, 70, 1)",
+      // borderColor: "rgba(219, 0, 0, 1)"
+    }
+  ];
+  
+  barChartOptions1: ChartOptions = {
+    responsive: true,
+  };
+  barChartLabels1: Label[] = ['SaleOffice', 'Inbound WB', 'Diesel Bay', 'Gasohol Bay', 'Outbound WB'];
+  barChartType1: ChartType = 'bar';
+  barChartLegend1 = true;
+  barChartPlugins1 = [];
+  
+  barChartData1: ChartDataSets[] = [
+    { data: [45, 37, 60, 10, 46, 33], 
+      label: 'Best Fruits',
+      backgroundColor: "rgba(100, 20, 45, 1)",
+      // borderColor: "rgba(219, 0, 0, 1)"
+    }
+  ];
 }

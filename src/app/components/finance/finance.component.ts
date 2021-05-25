8bd8cb7bbdf20ca3edb1d97ee276e5d330a155ca
vwaravy <Warachaya_v@hotmail.cm>
2021-05-25 22:05:20 +0700

    mai dai tum rai

--- src/app/components/finance/finance.component.ts
+++ src/app/components/finance/finance.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
 import { Label } from 'ng2-charts';
 
+
 @Component({
   selector: 'app-finance',
   templateUrl: './finance.component.html',
